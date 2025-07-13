32732c782483272f021be18489c4fdf0d2e4de0b
Sylvester Das <sylvester.das@gmail.com>
2025-07-13 12:12:09 +0000

    not attractive enough, maybe some kind of animation?

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,15 @@ export default {
             height: '0',
           },
         },
+        'background-pan': {
+          '0%': { backgroundPosition: '0% 50%' },
+          '100%': { backgroundPosition: '100% 50%' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'background-pan': 'background-pan 10s linear infinite alternate',
       },
     },
   },
