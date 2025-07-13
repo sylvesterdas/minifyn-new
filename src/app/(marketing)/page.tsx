cafa6258dc0c7958ca65bf4d364a77c2103b2cf3
Sylvester Das <sylvester.das@gmail.com>
2025-07-13 12:16:03 +0000

    instead of cursor what about mouse?

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -37,7 +37,6 @@ export default function Home() {
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      // In a real application, you might want to debounce this for performance
       const { clientX, clientY, currentTarget } = event;
       if (currentTarget instanceof Window) {
         const x = (clientX / currentTarget.innerWidth) * 100;
