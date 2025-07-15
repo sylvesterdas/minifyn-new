37f0a9f511215ef7eb829a0c35ee7c86c80e95ed
Sylvester Das <sylvester.das@gmail.com>
2025-07-15 11:27:08 +0000

    Also said that to serve images in next-gen format, but I am not sure if

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
