8be9d73bd3c663b104931b2dcdcf6ca8e82294be
Sylvester Das <sylvester.das@gmail.com>
2025-07-16 07:47:40 +0000

    Do we need to update the sitemap? will it be better to have multiple sit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,13 @@ export default function robots(): MetadataRoute.Robots {
   const siteUrl = 'https://www.minifyn.com';
   
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+        {
+            userAgent: '*',
+            allow: '/',
+            disallow: '/dashboard/',
+        }
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
